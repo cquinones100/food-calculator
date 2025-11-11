0df9c3cbe89a5785e8f258b71134bd140d114723
Carlos Quinones <cquinones100@gmail.com>
2025-11-11 12:57:21 -0500

    add fishery

---
+++ factories/food.ts
@@ -0,0 +1,25 @@
+import { Food } from "@/models/food";
+import { InferAttributes } from "sequelize";
+import { Factory } from "fishery";
+
+type FoodType = InferAttributes<Food>;
+
+export const foodFactory = Factory.define<Promise<Food>>(
+  async ({ sequence, onCreate }) => {
+    const attributes: FoodType = {
+      name: `food-${sequence}`,
+      calories: 100,
+      fat: 10,
+      carbs: 5,
+      protein: 5,
+    };
+
+    const food = Food.build(attributes);
+
+    onCreate(async () => {
+      return await food.save();
+    });
+
+    return food;
+  }
+);
