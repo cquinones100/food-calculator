a30127f299cee7e0f54569e3621916f7ee69cdf2
Carlos Quinones <cquinones100@gmail.com>
2025-11-11 20:58:49 -0500

    fix modal styling

--- src/modal.tsx
+++ src/modal.tsx
@@ -5,7 +5,7 @@ export default function Modal({
   onClose,
 }: PropsWithChildren<{ onClose: () => void }>) {
   return (
-    <div className="fixed inset-0 flex flex-col items-center bg-gray-900 bg-opacity-50 z-50 gap-2 p-2">
+    <div className="fixed inset-0 flex flex-col items-center bg-[#242424] bg-opacity-50 z-50 gap-2 p-2">
       <div className="flex justify-end w-full">
         <button className="rounded p-1" onClick={onClose}>
           <span className="text-white p-1 rounded flex text-xs">X</span>
