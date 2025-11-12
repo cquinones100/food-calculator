0a74ac567699d510c3f189bf8b498014bd331873
Carlos Quinones <cquinones100@gmail.com>
2025-11-11 20:38:16 -0500

    refactor form to use state

--- src/form.tsx
+++ src/form.tsx
@@ -1,23 +1,32 @@
 "use client";
 
+import { InferAttributes } from "sequelize";
 import saveFood from "./actions/saveFood";
 import {
+  ChangeEvent,
   FormEvent,
   PropsWithChildren,
   startTransition,
   useActionState,
   useEffect,
+  useRef,
   useState,
 } from "react";
+import { Food } from "./models/food";
+import { Entry } from "./models/entry";
 
 function Input({
   label,
   id,
   type,
+  value,
+  onChange,
 }: {
   label: string;
   id: string;
   type: "text" | "number";
+  value: string | number;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }) {
   return (
     <>
@@ -30,16 +39,33 @@ function Input({
         className="border p-2 rounded max-w-[150px]"
         id={id}
         name={id}
+        value={value}
+        onChange={onChange}
       />
     </>
   );
 }
 
-function Similarity({ children }: PropsWithChildren) {
-  return <li className="border p-2 rounded w-full text-center">{children}</li>;
+function Similarity({
+  children,
+  onClick,
+}: PropsWithChildren<{ onClick?: () => void }>) {
+  return (
+    <li className="border p-2 rounded w-full text-center" onClick={onClick}>
+      {children}
+    </li>
+  );
 }
 
-function SimilarNamesModal({ similarities }: { similarities: string[] }) {
+function SimilarNamesModal({
+  similarities,
+  onContinue,
+  onSelect,
+}: {
+  similarities: string[];
+  onContinue: () => void;
+  onSelect: (name: string) => void;
+}) {
   const [render, setRender] = useState(similarities.length > 0);
 
   useEffect(() => {
@@ -72,9 +98,20 @@ function SimilarNamesModal({ similarities }: { similarities: string[] }) {
       </div>
       <ul className="flex flex-col w-full gap-2">
         {similarities.map((similarity) => {
-          return <Similarity key={similarity}>{similarity}</Similarity>;
+          return (
+            <Similarity
+              key={similarity}
+              onClick={() => {
+                onSelect(similarity);
+              }}
+            >
+              {similarity}
+            </Similarity>
+          );
         })}
-        <Similarity key="continue">Continue as is</Similarity>
+        <Similarity key="continue" onClick={onContinue}>
+          Continue as is
+        </Similarity>
       </ul>
     </div>
   );
@@ -85,24 +122,54 @@ export default function Form() {
     similarities: [],
   };
 
+  const [pendingFood, setPendingFood] = useState<
+    InferAttributes<Food> & Omit<InferAttributes<Entry>, "food" | "foodId">
+  >({
+    name: "",
+    calories: 0,
+    carbs: 0,
+    fat: 0,
+    protein: 0,
+    servingSize: 0,
+    totalWeight: 0,
+    date: new Date(),
+  });
+
   async function onSubmit(
     _previousState: { similarities: string[] },
     formData: FormData
   ) {
-    const res = await saveFood({
-      name: String(formData.get("food-name"))!,
-      calories: Number(formData.get("calories")!),
-      carbs: Number(formData.get("carbs")!),
-      fat: Number(formData.get("fat")!),
-      protein: Number(formData.get("protein")!),
-      servingSize: Number(formData.get("serving-size")!),
-      totalWeight: Number(formData.get("total-weight")!),
-      date: new Date(),
+    let name = formData.get("food-name");
+
+    const { calories, carbs, fat, protein, servingSize, totalWeight, date } =
+      pendingFood;
+    name = name ? String(name) : pendingFood.name;
+    const forceSimilarity = formData.get("force-similarity") === "true";
+
+    setPendingFood({
+      ...pendingFood,
+      name,
     });
+
+    const res = await saveFood(
+      {
+        name,
+        calories,
+        carbs,
+        fat,
+        protein,
+        servingSize,
+        totalWeight,
+        date,
+      },
+      {
+        forceSimilarity,
+      }
+    );
 
     if (res?.error) {
       return {
-        similarities: res.similarities,
+        similarities: res.similarities || [],
       };
     } else {
       return initialState;
@@ -114,7 +181,30 @@ export default function Form() {
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    const formData = new FormData(event.currentTarget);
+    const formData = new FormData();
+
+    startTransition(() => {
+      formAction(formData);
+    });
+  }
+
+  const formRef = useRef<HTMLFormElement>(null);
+
+  function handleContinue() {
+    const formData = new FormData();
+
+    formData.append("forceSimilarity", "true");
+
+    startTransition(() => {
+      formAction(formData);
+    });
+  }
+
+  function handleSelect(name: string) {
+    const formData = new FormData();
+
+    formData.append("food-name", name);
+    formData.append("force-similarity", "true");
 
     startTransition(() => {
       formAction(formData);
@@ -123,18 +213,88 @@ export default function Form() {
 
   return (
     <>
-      <SimilarNamesModal similarities={state.similarities} />
+      <SimilarNamesModal
+        similarities={state.similarities}
+        onContinue={handleContinue}
+        onSelect={handleSelect}
+      />
       <form
+        ref={formRef}
         onSubmit={handleSubmit}
         className="flex flex-wrap gap-2 justify-center align-middle"
       >
-        <Input label="Food Name" id="food-name" type="text" />
-        <Input label="Total Weight (g)" id="total-weight" type="number" />
-        <Input label="Serving Size (g)" id="serving-size" type="number" />
-        <Input label="Calories" id="calories" type="number" />
-        <Input label="Fat (g)" id="fat" type="number" />
-        <Input label="Carbs (g)" id="carbs" type="number" />
-        <Input label="Protein (g)" id="protein" type="number" />
+        <Input
+          label="Food Name"
+          id="food-name"
+          type="text"
+          value={pendingFood.name}
+          onChange={(e) => {
+            setPendingFood({ ...pendingFood, name: e.target.value });
+          }}
+        />
+        <Input
+          label="Total Weight (g)"
+          id="total-weight"
+          type="number"
+          value={pendingFood.totalWeight}
+          onChange={(e) => {
+            setPendingFood({
+              ...pendingFood,
+              totalWeight: Number(e.target.value),
+            });
+          }}
+        />
+        <Input
+          label="Serving Size (g)"
+          id="serving-size"
+          type="number"
+          value={pendingFood.servingSize}
+          onChange={(e) => {
+            setPendingFood({
+              ...pendingFood,
+              servingSize: Number(e.target.value),
+            });
+          }}
+        />
+        <Input
+          label="Calories"
+          id="calories"
+          type="number"
+          value={pendingFood.calories}
+          onChange={(e) => {
+            setPendingFood({
+              ...pendingFood,
+              calories: Number(e.target.value),
+            });
+          }}
+        />
+        <Input
+          label="Fat (g)"
+          id="fat"
+          type="number"
+          value={pendingFood.fat}
+          onChange={(e) => {
+            setPendingFood({ ...pendingFood, fat: Number(e.target.value) });
+          }}
+        />
+        <Input
+          label="Carbs (g)"
+          id="carbs"
+          type="number"
+          value={pendingFood.carbs}
+          onChange={(e) => {
+            setPendingFood({ ...pendingFood, carbs: Number(e.target.value) });
+          }}
+        />
+        <Input
+          label="Protein (g)"
+          id="protein"
+          type="number"
+          value={pendingFood.protein}
+          onChange={(e) => {
+            setPendingFood({ ...pendingFood, protein: Number(e.target.value) });
+          }}
+        />
         <input
           disabled={isPending}
           type="submit"
