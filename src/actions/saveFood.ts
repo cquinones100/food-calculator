717b590149b5f7958764541cb45fcbe17a6b3b4a
Carlos Quinones <cquinones100@gmail.com>
2025-11-11 19:29:51 -0500

    revalidate and redirect after saving food

--- src/actions/saveFood.ts
+++ src/actions/saveFood.ts
@@ -5,6 +5,8 @@ import { Entry } from "@/models/entry";
 import { InferAttributes } from "sequelize";
 import initializeDb from "@/database";
 import { isSimilar, similarityScore } from "../../lib/isSimilar";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 class FoodIsSimilarError extends Error {}
 
@@ -75,6 +77,7 @@ async function saveFood(
     );
 
     await transaction.commit();
+    revalidatePath("/");
   } catch (e) {
     await transaction.rollback();
     if (e instanceof FoodIsSimilarError) {
@@ -95,6 +98,8 @@ async function saveFood(
 
     throw e;
   }
+
+  redirect("/");
 }
 
 export default saveFood;
