d39a33028f86190a0ab985f8bc3413e08a73fae4
Carlos Quinones <cquinones100@gmail.com>
2026-01-15 17:45:28 -0500

    use isSimilar in getFoodsByName

--- src/actions/getFoodsByName.ts
+++ src/actions/getFoodsByName.ts
@@ -1,13 +1,19 @@
+"use server";
+
 import initializeDb from "@/database";
-import { similarityScore } from "../../lib/isSimilar";
+import { isSimilar, similarityScore } from "../../lib/isSimilar";
 
 export default async function getFoodsByName(name: string) {
   const db = await initializeDb();
   const allFoods = await db.selectFrom("Foods").selectAll().execute();
 
-  const ordered = allFoods.sort((a, b) => {
-    return similarityScore(name, b.name) - similarityScore(name, a.name);
-  });
+  const ordered = allFoods
+    .filter((food) => {
+      return isSimilar(name, food.name);
+    })
+    .sort((a, b) => {
+      return similarityScore(name, b.name) - similarityScore(name, a.name);
+    });
 
   return ordered;
 }
