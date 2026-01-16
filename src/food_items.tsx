5dc7d512e034d31630d41f9583952c5ad2316bd3
Carlos Quinones <cquinones100@gmail.com>
2026-01-15 21:32:25 -0500

    make ingredients deletable

--- src/food_items.tsx
+++ src/food_items.tsx
@@ -4,14 +4,20 @@ import { use, useState } from "react";
 import { Ingredient, IngredientsContext } from "./app/IngredientsContext";
 import Form from "./app/ingredients/new/form";
 
-function FoodItem({ ingredient }: { ingredient: Ingredient }) {
+function FoodItem({
+  ingredient,
+  onDeleteIngredient,
+}: {
+  ingredient: Ingredient;
+  onDeleteIngredient: () => void;
+}) {
   return (
     <div className="border p-2 rounded mb-2 w-full flex justify-between">
       <div className="flex flex-col w-full">
         <div className="flex justify-end"></div>
         <div className="flex gap-2 justify-between flex-wrap items-center">
           <div className="font-bold text-xl flex-1">{ingredient.name}</div>
-          <button className="rounded p-1">
+          <button className="rounded p-1" onClick={onDeleteIngredient}>
             <span className="text-white p-1 rounded flex text-xs">X</span>
           </button>
         </div>
@@ -35,11 +41,25 @@ export default function FoodItems() {
     setIngredients([...ingredients, food]);
   }
 
+  function onDeleteIngredient(food: Ingredient) {
+    setIngredients(
+      ingredients.filter((ingredient) => {
+        return food.id !== ingredient.id;
+      })
+    );
+  }
+
   return (
     <div className="flex flex-col items-center gap-2 w-full">
       <div className="flex w-full items-end justify-end"></div>
       {ingredients.map((ingredient, index) => (
-        <FoodItem key={index} ingredient={ingredient} />
+        <FoodItem
+          key={index}
+          ingredient={ingredient}
+          onDeleteIngredient={() => {
+            onDeleteIngredient(ingredient);
+          }}
+        />
       ))}
       <Form onFoodSubmit={onFoodSubmit} />
     </div>
