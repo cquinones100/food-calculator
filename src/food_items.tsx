fd2c6bd3d7dfffb61456faed18d3825f96356a3b
Carlos Quinones <cquinones100@gmail.com>
2026-01-15 21:24:40 -0500

    fix styling for ingredients

--- src/food_items.tsx
+++ src/food_items.tsx
@@ -7,16 +7,16 @@ import Form from "./app/ingredients/new/form";
 function FoodItem({ ingredient }: { ingredient: Ingredient }) {
   return (
     <div className="border p-2 rounded mb-2 w-full flex justify-between">
-      <div className="flex flex-col">
+      <div className="flex flex-col w-full">
         <div className="flex justify-end"></div>
         <div className="flex gap-2 justify-between flex-wrap items-center">
           <div className="font-bold text-xl flex-1">{ingredient.name}</div>
-          <div>Total Weight: {ingredient.totalWeight}g</div>
           <button className="rounded p-1">
             <span className="text-white p-1 rounded flex text-xs">X</span>
           </button>
         </div>
-        <div className="flex gap-2 flex-wrap">
+        <div className="flex gap-2 flex-wrap justify-between">
+          <div>Total Weight: {ingredient.totalWeight}g</div>
           <div>Serving Size: {ingredient.servingSize}g</div>
           <div>Calories: {ingredient.calories}</div>
           <div>Fat: {ingredient.fat}g</div>
